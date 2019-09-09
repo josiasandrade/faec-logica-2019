522cf07797c661118ea85f05c39c37d320d62ffd
josiasandrade <55114250+josiasandrade@users.noreply.github.com>
2019-09-09 20:53:41 -0300

    Add files via upload
    primeiro teste.

---
+++ CartaoPonto.java
@@ -0,0 +1,33 @@
+public class CartaoPonto {
+	
+	int entradaM;
+	int saidaM;
+	int entradaT;
+	int saidaT;
+	int totalMes;
+	
+	void inicializaMes(CartaoPonto[] dia){
+		for ( int i=0; i<=30; i++){
+			dia[i]= new CartaoPonto();
+		}
+	}
+	void imprimeRegistroMensal(CartaoPonto[] 2){
+		for ( int i=0; i<=30; i++){
+			dia[2]= new CartaoPonto();
+		}
+	}
+	
+	public static void main(String[] args) {
+		CartaoPonto arquivoMensal = new CartaoPonto();
+		arquivoMensal.inicializaMes(dia);
+		
+		CartaoPonto[]dia = new CartaoPonto[30];	
+		
+			dia[i]=entradaM = 8;
+			dia[i]=saidaM = 11;
+			dia[i]=entradaT = 12;
+			dia[i]=saidaT = 17;
+		
+	}
+	
+}
